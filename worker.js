730e5020050a07054494eafc51992a2215948a19
Vivek Jishtu <github@vivekjishtu.com>
2016-09-19 19:53:17 +0530

    Added a port variable

--- worker.js
+++ worker.js
@@ -1,6 +1,8 @@
+var ports = [];
+
 onconnect = function(e) {
   var port = e.ports[0];
-
+  ports.push(port);
   port.onmessage = function(e) {
     var workerResult = 'Result: ' + (e.data[0] * e.data[1]);
     port.postMessage(workerResult);
